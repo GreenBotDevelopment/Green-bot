0fd609697625e7ae35aa9a155966dec0beaefbbb
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-16 17:33:06 +0100

    Update profil.js

--- commands/rpg/profil.js
+++ commands/rpg/profil.js
@@ -13,23 +13,32 @@ module.exports = {
     cat: 'rpg',
     async execute(message, args) {
         const member = message.mentions.users.first() || message.author;
-
+        if (member.bot) return message.channel.send(`${emoji.error} Le profil d'un bot ? sérieusement ?`)
         let advdb = await adventure.find({ UserID: member.id })
         let userldb = await UserRpg.findOne({ UserID: member.id })
-
+        let argent;
+        if (userldb) {
+            if (userldb.credits) {
+                argent = userldb.credits;
+            } else {
+                argent = '0';
+            }
+        } else {
+            argent = '0';
+        }
         const reportEmbed = new Discord.MessageEmbed()
             .setTitle(`Profil de \`${member.username}\``)
             .setAuthor(member.tag, member.displayAvatarURL({ dynamic: true, size: 512 }))
 
         .addField(`${emoji.quest} Quêtes`, `${member.username} a ${advdb.length} aventures en cours ou démarrées. `)
            
-            .addField("Argent", `0`,true)
-            .addField("Réputation", `0`,true)
+            .addField(`${emoji.money} Argent`, argent, true)
+            .addField("Réputation", `0`, true)
 
 
-        .setFooter(message.client.footer)
+        .setFooter(client.footer)
 
-        .setColor(message.client.color);
+        .setColor(client.color);
         message.channel.send(reportEmbed);
 
 
