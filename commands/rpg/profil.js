709b178883cfa57ac1d472c0398178764f2e579f
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-16 17:46:41 +0100

    Update profil.js

--- commands/rpg/profil.js
+++ commands/rpg/profil.js
@@ -1,47 +1,170 @@
 const fetch = require("node-fetch");
 const Discord = require('discord.js');
 const emoji = require('../../emojis.json')
-
+const math = require('mathjs');
 const adventure = require("../../database/models/adventure");
 const UserRpg = require("../../database/models/userrpg");
 module.exports = {
-    name: 'profil',
-    description: 'Affiche votre profil sur le rpg',
-    exemple: '@pauldB09',
-    usage: '[utilisateur]',
+    name: 'work',
+    description: 'effectue du travail en fonction de votre profil',
+    cooldown: 10000,
     botpermission: ['EMBED_LINKS'],
     cat: 'rpg',
+    adventure: true,
     async execute(message, args) {
-        const member = message.mentions.users.first() || message.author;
-        if (member.bot) return message.channel.send(`${emoji.error} Le profil d'un bot ? sérieusement ?`)
-        let advdb = await adventure.find({ UserID: member.id })
-        let userldb = await UserRpg.findOne({ UserID: member.id })
-        let argent;
-        if (userldb) {
-            if (userldb.credits) {
-                argent = userldb.credits;
-            } else {
-                argent = '0';
-            }
-        } else {
-            argent = '0';
-        }
-        const reportEmbed = new Discord.MessageEmbed()
-            .setTitle(`Profil de \`${member.username}\``)
-            .setAuthor(member.tag, member.displayAvatarURL({ dynamic: true, size: 512 }))
 
-        .addField(`${emoji.quest} Quêtes`, `${member.username} a ${advdb.length} aventures en cours ou démarrées. `)
-           
-            .addField(`${emoji.money} Argent`, argent, true)
-            .addField("Réputation", `0`, true)
+        let advdb = await adventure.findOne({ UserID: message.author.id, active: true })
+
+        limit = 15;
+        const n = Math.floor(Math.random() * limit + 1);
+        const reportEmbed = new Discord.MessageEmbed()
+
+        .setAuthor(`TRAVAIL [${advdb.profil}]`)
+
+        .setDescription(`Le soleil est écrasant ☀ mais vous avez travaillé avec acharnement et votre maître vous donne votre salaire , bien mérité
+        **Maitre** : Je suis fier de toi ${message.author.username} , tu as bien travaillé !
+        **${message.author.username}**: merci maître !
+        **Maitre** : Voici ta paye :  ${n} crédits.
+         
+        Voulez vous contester la paye ou accepter ce que vous donne votre maître ?
+        ✅ : accepter la paye.
+        😒 : contester.
+        Attention , choissisez bien.....
+
+        `)
 
 
         .setFooter(message.client.footer)
 
         .setColor(message.client.color);
-        message.channel.send(reportEmbed);
+        message.channel.send(reportEmbed).then(m => {
+            m.react("✅")
+            m.react("😒")
+
+
+            const filtro = (reaction, user) => {
+                return user.id == message.author.id;
+            };
+            m.awaitReactions(filtro, {
+                max: 1,
+                time: 40000,
+                errors: ["time"]
+            }).catch(() => {
+
+                const errorEmbed = new Discord.MessageEmbed()
 
 
 
+                .setDescription(`${emoji.error} Erreur : temps écoulé ! `)
+
+
+                .setFooter(message.client.footer)
+
+                .setColor("#982318");
+                m.edit(errorEmbed);
+            }).then(async(coleccionado) => {
+
+                const reaccion = coleccionado.first();
+                if (reaccion.emoji.name === "✅") {
+                    limit = 5;
+                    const exp = Math.floor(Math.random() * limit + 1);
+                    if (exp === 2) {
+                        const embed = new Discord.MessageEmbed()
+
+
+                        .setAuthor(`TRAVAIL [suite]`)
+
+                        .setDescription(`
+                     **${message.author.username}**: merci maître , je prend cette paye volontiers !
+                    **Maitre** : tu es un bon serviteur et , pour te récompenser , je vais te donner ${n + 5} crédits.
+                    **${message.author.username}**: comment vous remercier maitre !
+                    **Maitre** : en continuant ton travail.
+                    ${emoji.succes} ${n + 5} crédits on étés ajoutés à votre compte avec succès
+                    `)
+
+
+                        .setFooter(message.client.footer)
+
+                        .setColor(message.client.color);
+                        let final = n + 5;
+                        let useldb = await UserRpg.findOne({ UserID: message.author.id })
+                        if (useldb) {
+                            let act;
+                            if (useldb.credits) {
+                                act = useldb.credits;
+                            } else {
+                                act = 0;
+                            }
+                            let newfric = math.evaluate(`${act} + ${final}`);
+                            const newcbio = await UserRpg.findOneAndUpdate({ UserID: message.author.id }, { $set: { credits: newfric } }, { new: true });
+
+                        } else {
+                            const verynew = new UserRpg({
+                                UserID: `${message.author.id}`,
+                                credits: `${final}`,
+
+                            }).save();
+                        }
+                        m.delete();
+
+                        message.channel.send(embed);
+                    } else {
+                        const embed = new Discord.MessageEmbed()
+
+
+                        .setAuthor(`TRAVAIL [suite]`)
+
+                        .setDescription(`
+                     **${message.author.username}**: merci maître , je prend cette paye volontiers !
+                    **Maitre** : Bien , prend tes  ${n} crédits.
+                    ${emoji.succes} ${n} crédits on étés ajoutés à votre compte avec succès`)
+
+
+                        .setFooter(message.client.footer)
+
+                        .setColor(message.client.color);
+                        m.delete();
+                        let final = n;
+                        let useldb = await UserRpg.findOne({ UserID: message.author.id })
+                        if (useldb) {
+                            console.log('exits profil');
+                            let act;
+                            if (useldb.credits) {
+                                act = useldb.credits;
+                                console.log('exits');
+                            } else {
+                                act = 0;
+                            }
+                            let newfric = math.evaluate(`${act} + ${n}`);
+                            const newcbio = await UserRpg.findOneAndUpdate({ UserID: message.author.id }, { $set: { credits: newfric } }, { new: true });
+
+                        } else {
+                            const verynew = new UserRpg({
+                                UserID: `${message.author.id}`,
+                                credits: `${n}`,
+
+                            }).save();
+
+                        }
+                        message.channel.send(embed);
+                    }
+
+                }
+                if (reaccion.emoji.name === "😒") {
+                    const errorEmbed = new Discord.MessageEmbed()
+
+
+
+                    .setDescription(`${emoji.error} Erreur : temps écoulé ! `)
+
+
+                    .setFooter(message.client.footer)
+
+                    .setColor("#982318");
+                    m.edit(errorEmbed);
+                }
+            });
+        });
     },
+
 };
