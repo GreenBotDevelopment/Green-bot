421914112d04e93c773e4076ddf7aec7368cac88
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-16 18:29:25 +0100

    Create bal.js

---
+++ commands/rpg/bal.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch");
+const Discord = require('discord.js');
+const emoji = require('../../emojis.json')
+
+const partner = require("../../database/models/partner");
+module.exports = {
+    name: 'bal',
+    description: 'Affiche tous les serveurs partenaires que vous pouvez rejoindre pour gagner des crédits',
+
+    aliases: ['ballet'],
+    cat: 'rpg',
+    async execute(message, args) {
+        const { client } = message;
+        let channeldb = await partner.find({})
+
+        const reportEmbed = new Discord.MessageEmbed()
+            .setTitle(`\`✨\` Serveurs pour gagner de l'argent `)
+            .setDescription(`Voici tous les serveurs (${channeldb.length}) ou tu gagneras de l'argent en les rejoignants . Si tu souhaite voir ton serveur ici , il suffit de gagner 100 crédits.`)
+
+        .setFooter(client.footer)
+
+        .setColor(client.color);
+        if(channeldb.length === 0){
+            reportEmbed.addField('Aucuns serveurs', `sorry`)
+        }else{
+            channeldb.forEach(async(server) => {
+                let guild = message.client.guilds.get(server.serverID);
+     
+                 reportEmbed.addField(guild.name, `${guild.membercount} membres`)
+     
+             });
+        }
+        
+
+
+
+
+        message.channel.send(reportEmbed);
+
+
+
+    },
+};
