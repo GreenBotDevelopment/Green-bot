5ac8071c212403e7ee7b5ca1c7d45954a8afa3e4
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-21 12:18:57 +0100

    Add files via upload

---
+++ commands/fun/jail.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+const Discord = require('discord.js');
+const emoji = require('../../emojis.json')
+const DIG = require("discord-image-generation");
+module.exports = {
+    name: 'jail',
+    description: 'Ajoute des effets à votre avatar',
+    botpermissions: ['ATTACH_FILES'],
+    usage: '[user]',
+    cat: 'fun',
+    async execute(message, args) {
+        const user = message.mentions.users.first() || message.author;
+        let avatar = user.displayAvatarURL({ dynamic: false, format: 'png' });
+
+        let img = await new DIG.Jail().getImage(avatar)
+
+        let attach = new Discord.MessageAttachment(img, "delete.png");;
+        message.channel.send(attach)
+
+
+    },
+};
