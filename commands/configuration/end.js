8ff1423aacfccacd7b6985360efd4bb56cc933d3
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-21 12:57:19 +0100

    Create end.js

---
+++ commands/configuration/end.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const Welcome = require('../../database/models/Welcome')
+const emoji = require('../../emojis.json')
+
+module.exports = {
+    name: 'interchat-end',
+    description: 'Arrete le système d\'interchat',
+    aliases: ['endinterchat'],
+    cat: 'inter',
+
+    guildOnly: true,
+
+    permissions: ['MANAGE_GUILD'],
+    async execute(message, args) {
+
+        const verify = await Welcome.findOne({ serverID: message.guild.id, reason: `interchat-s` })
+        if (verify) {
+            const newserver = await Welcome.findOneAndDelete({ serverID: message.guild.id, reason: `interchat-s` });
+            const newserver = await Welcome.findOneAndDelete({ channelID: message.guild.id, reason: `interchat-s` });
+
+            return message.channel.send(`${emoji.succes} j'ai mis fin à l'interchat avec succès.`);
+
+
+        } else {
+
+            return message.channel.send(`${emoji.error} Vous devez avoir une configuration pour la supprimer..`);
+
+        }
+
+
+
+
+
+    },
+};
