559a6c32bca18d6a227c21a4383760fa95b6f8e7
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-22 17:09:49 +0100

    Update sondage.js

--- commands/sondage.js
+++ commands/sondage.js
@@ -1,6 +1,7 @@
 const { prefix } = require('../config.json');
 const Discord = require('discord.js');
 const emoji = require('../emojis.json')
+const ms = require('ms');
 module.exports = {
     name: 'sondage',
     description: 'Crée un sondage',
@@ -8,67 +9,63 @@ module.exports = {
     args: true,
     cat: 'util',
     usage: '<temps> <contenu>',
-    exemple: 'Un nouveau salon 1m',
+    exemple: ' 1m Un nouveau salon',
     permissions: `MENTION_EVERYONE`,
     cooldown: 5,
-  async  execute(message, args) {
+    async execute(message, args) {
 
 
 
-      
-        let time = args[1];
-        let question = args.slice(2).join(" ");
-        if(!question) return message.channel.send(`${emoji.error} Veuillez mettre une question`)
-        let regex = new RegExp(/^([0-9]{2}|[0-9]{1})[sSmM]$/);
-        if (regex.test(time)) {
-            if (time.toLowerCase().endsWith('s')) {
-                time = parseInt(time.substring(0, time.indexOf('s')));
-                time *= 1000;
-            } else if (time.toLowerCase().endsWith('m')) {
-                time = parseInt(time.substring(0, time.indexOf('m')));
-                time *= 60 * 1000;
-            }
-            const embed = new MessageEmbed()
-                .setTitle(question)
-                .setDescription('Réagissez 👍 ou 👎')
-                .setColor(message.client.color)
-                .setDescription(`Sondage de ${message.author.tag}`, message.author.displayAvatarUrl())
-            try {
-                const polls = new Map();
-                const userVotes = new Map();
-                let filter = (reaction, user) => {
-                    if (user.bot) return false;
-                    if (['👍', '👎'].includes(reaction.emoji.name)) {
-                        if (polls.get(reaction.message.id).get(user.id))
-                            return false;
-                        else {
-                            userVotes.set(user.id, reaction.emoji.name);
-                            return true;
-                        }
+
+
+        if (!args[0]) return message.channel.send(`${emoji.error} Veuillez fournir une durée valide (s/m/h/d)`);
+        let time = ms(args[1]);
+        if (!time || time > 10000)
+            return message.channel.send(`${emoji.error} Veuillez fournir une durée valide (s/m/h/d)`);
+        let question = args.slice(1).join(" ");
+        if (!question) return message.channel.send(`${emoji.error} Veuillez mettre une question`)
+
+        const embed = new MessageEmbed()
+            .setTitle(question)
+            .setDescription('Réagissez 👍 ou 👎')
+            .setColor(message.client.color)
+            .setDescription(`Sondage de ${message.author.tag}`, message.author.displayAvatarUrl())
+        try {
+            const polls = new Map();
+            const userVotes = new Map();
+            let filter = (reaction, user) => {
+                if (user.bot) return false;
+                if (['👍', '👎'].includes(reaction.emoji.name)) {
+                    if (polls.get(reaction.message.id).get(user.id))
+                        return false;
+                    else {
+                        userVotes.set(user.id, reaction.emoji.name);
+                        return true;
                     }
                 }
-                let msg = await message.channel.send(embed);
-                await msg.react('👍');
-                await msg.react('👎');
-                polls.set(msg.id, userVotes);
-                let reactions = await msg.awaitReactions(filter, { time: time });
-                let thumbsUp = reactions.get('👍');
-                let thumbsDown = reactions.get('👎');
-                let thumbsUpResults = 0,
-                    thumbsDownResults = 0;
-                if (thumbsUp)
-                    thumbsUpResults = thumbsUp.users.cache.filter(u => !u.bot).size;
-                if (thumbsDown)
-                    thumbsDownResults = thumbsDown.users.cache.filter(u => !u.bot).size;
-                const resultsEmbed = new MessageEmbed()
-                    .setTitle('Resultats')
-                    .setColor(message.client.color)
-                    .setDescription(`👍 - ${thumbsUpResults} votes\n\n👎 - ${thumbsDownResults} votes\n`);
-                await message.channel.send(resultsEmbed);
-            } catch (err) {
-                console.log(err);
             }
+            let msg = await message.channel.send(embed);
+            await msg.react('👍');
+            await msg.react('👎');
+            polls.set(msg.id, userVotes);
+            let reactions = await msg.awaitReactions(filter, { time: time });
+            let thumbsUp = reactions.get('👍');
+            let thumbsDown = reactions.get('👎');
+            let thumbsUpResults = 0,
+                thumbsDownResults = 0;
+            if (thumbsUp)
+                thumbsUpResults = thumbsUp.users.cache.filter(u => !u.bot).size;
+            if (thumbsDown)
+                thumbsDownResults = thumbsDown.users.cache.filter(u => !u.bot).size;
+            const resultsEmbed = new MessageEmbed()
+                .setTitle('Resultats')
+                .setColor(message.client.color)
+                .setDescription(`👍 - ${thumbsUpResults} votes\n\n👎 - ${thumbsDownResults} votes\n`);
+            await message.channel.send(resultsEmbed);
+        } catch (err) {
+            console.log(err);
         }
+
 
 
 
