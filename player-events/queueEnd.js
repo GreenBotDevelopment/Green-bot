3f9b0b8e6947d55101c9bd17c4b46cdb11956b8d
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-24 11:37:21 +0100

    Update queueEnd.js

--- player-events/queueEnd.js
+++ player-events/queueEnd.js
@@ -1,5 +1,4 @@
 const emoji = require('../emojis.json');
-
 module.exports = (client, message, queue) => {
 
     message.channel.send(`${emoji.error} - J'ai arreté la musique car la queue est vide  !`);
