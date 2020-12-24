558479e468175a113dc4af42982b98331e6981d8
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-24 12:19:57 +0100

    Create pause.js

---
+++ commands/music/pause.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+
+const emoji = require('../../emojis.json')
+
+module.exports = {
+    name: 'pause',
+    description: 'Met la musique actuelle en pause',
+    cat: 'musique',
+
+    botpermissions: ['CONNECT', 'SPEAK'],
+
+    async execute(message, args) {
+
+
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            return message.channel.send(`${emoji.error} Vous devez d'abord rejoindre un salon vocal !`)
+        }
+
+        if (!message.client.player.getQueue(message)) return message.channel.send(`${emoji.error} Je ne joue pas de musique actuellement.`)
+        message.client.player.pause(message);
+
+        message.channel.send(`${emoji.success} - La musique a été mise en pause  !`);
+
+
+
+
+
+
+
+
+
+
+
+
+    },
+};
