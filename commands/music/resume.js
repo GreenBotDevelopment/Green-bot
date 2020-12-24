8116724d72306ce87da398883bfa1380800ef04e
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-24 12:20:58 +0100

    Create resume.js

---
+++ commands/music/resume.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+
+const emoji = require('../../emojis.json')
+
+module.exports = {
+    name: 'resume',
+    description: 'Remet la musique actuelle',
+    cat: 'musique',
+
+    botpermissions: ['CONNECT', 'SPEAK'],
+
+    async execute(message, args) {
+
+
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            return message.channel.send(`${emoji.error} Vous devez d'abord rejoindre un salon vocal !`)
+        }
+
+        if (!message.client.player.getQueue(message)) return message.channel.send(`${emoji.error} Je ne joue pas de musique actuellement.`)
+        message.client.player.resume(message);
+
+        message.channel.send(`${emoji.success} - La musique reprend....`);
+
+
+
+
+
+
+
+
+
+
+
+
+    },
+};
