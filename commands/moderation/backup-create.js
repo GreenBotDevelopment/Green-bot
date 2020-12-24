91d245b5bf7d32a1164d1b2bb4c4d2bfa4b48aea
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-24 16:51:40 +0100

    Create backup-create.js

---
+++ commands/moderation/backup-create.js
@@ -0,0 +1,54 @@
+const backup = require("discord-backup");
+const emoji = require('../../emojis.json')
+module.exports = {
+    name: 'backup-create',
+    description: 'crée une backup du seveur',
+    botpermissons: ['MANAGE_GUILD'],
+    permissions: ['ADMINISTRATOR'],
+    aliases: ['b-create'],
+    cooldown: 30000,
+    cat: 'utilities',
+    execute(message, args) {
+
+        const client = message.client;
+        const unmuteEmbed = new Discord.MessageEmbed()
+            .setTitle('Création de la backup...')
+            .setDescription(`Enregistrement de :
+            **${message.guild.channels.cache.size}** salons 
+           ** ${message.guild.roles.cache.size}** Rôles
+            **${message.guild.emojis.cache.size}** salons
+            -Du nom du serveur et de l'icone.
+
+            `)
+            .setFooter(message.client.footer)
+            .setColor(message.client.color);
+        message.channel.send(unmuteEmbed).then(m=>{
+            backup.create(message.guild, {
+                jsonBeautify: true
+            }).then((backupData) => {
+
+                const unmuteEmbedee = new Discord.MessageEmbed()
+                .setTitle('Backup crée avec succès.')
+                .setDescription(`***Pour la charger :**
+                \`\`\`*backup-load ${backupData.id}\`\`\`
+                ***Info :**
+                \`\`\`*backup-info ${backupData.id}\`\`\`
+                `)
+                .setFooter(message.client.footer)
+                .setColor(message.client.color);
+            message.author.send(unmuteEmbedee)
+            const unmuteEmbede = new Discord.MessageEmbed()
+                .setTitle('Backup crée avec succès.')
+                .setDescription(`Vous avez reçu les informations en MP 😉`)
+                .setFooter(message.client.footer)
+                .setColor(message.client.color);
+            
+              m.edit(unmuteEmbede);
+            
+            });
+        })
+      
+   
+
+},
+};
