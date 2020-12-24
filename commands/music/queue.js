3529cfedf249452677b83e6043e57e7a9a607647
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2020-12-24 11:40:18 +0100

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -21,7 +21,7 @@ module.exports = {
         if (!message.client.player.getQueue(message)) return message.channel.send(`${emoji.error} Je ne joue pas de musique actuellement.`)
 
 
-        const queue = client.player.getQueue(message);
+        const queue = message.client.player.getQueue(message);
         const embed = new Discord.MessageEmbed()
 
         .setTitle(`Queue du serveur (${queue.tracks.length} sons)`)
