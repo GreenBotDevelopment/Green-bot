bdd4d7cec1844d4c15e17707224026a24994af8b
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2021-02-28 18:36:00 +0100

    📝Modification : stats

--- commands/utilities/botinfo.js
+++ commands/utilities/botinfo.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const moment = require('moment')
+const guild = require('../../database/models/guild');
+const backup = require('../../database/models/backup');
+const sugg = require('../../database/models/sugg');
+
 module.exports = {
     name: 'botinfo',
     description: 'Affiche des informations concernant le bot.',
@@ -7,6 +11,11 @@ module.exports = {
     cat: 'utilities',
 
     async execute(message, args) {
+        let commandOnSErver = await guild.find({ serverID: message.guild.id, reason: `command` })
+        let commands = await guild.find({ reason: `command` })
+        const currentGiveaways = message.client.manager.giveaways.length;
+        const check = await backup.find({})
+        let suggs = await sugg.find({})
 
         const embed = new Discord.MessageEmbed()
 
@@ -14,10 +23,9 @@ module.exports = {
             .setDescription(`
             **${message.client.user.username}** Vient du bot [\`Green-bot\`](https://github.com/pauldb09/Green-bot/) Un bot open source créer par [\`Pauldb09\`](https://github.com/pauldb09).
 
-
            🆔\`Identifiant\` : **${message.client.user.id}**
            👤\`Compte Crée le :\` : **${moment(message.client.user.createdTimestamp).locale('fr').format('LT ,')} ${moment(message.client.user.createdTimestamp).locale('fr').format('LL, ')} ${moment(message.client.user.createdTimestamp).locale('fr').fromNow()}**
-           ⏺\`Bot certifié ? :\` : **${message.client.user.verified ? 'Non 😕': 'oui 😹'}**
+           ⏺\`Bot certifié ? :\` : **Oui **
            
 
 
@@ -28,18 +36,24 @@ module.exports = {
             👨‍👩‍👦\`Utilisateurs\` : **${message.client.users.cache.size}**
             🧾\`Commandes\` : **${message.client.commands.size}**
             🆚\`Version\` : **1.4.8**
-            `)
-            .addField(`🤖 __Informations :__`, `
-            💻\`Language\` : **Discord.js**
+            `, true)
+
+        .addField(`🤖 __Informations :__`, `
+            📉\`Language\` : **Discord.js**
             👨🏼‍💻\`Développeur\` : **<@${message.client.owner}>**
             💾\`Base de données\` : **MongoDB**
-           \`Merci à :\` : **Universe Tech YT#0077** , **!Zerio.js#0001**
+            🖥\`Hébergeur\` : **AdkyNet**
+            `, true)
+            .addField(`📊 __Utilisation__`, `
+            💻\`Personnes différentes ayant fait des commandes\` : **${commands.length}** (**${commandOnSErver.length}** sur ce serveur)
+            🎁\`Giveaways\` : **${currentGiveaways}**
+            🥡\`Sauvegardes crées\` : **${check.length}**
+            💡\`Suggestions\` : **${suggs.length}**
             `)
-
-        .setColor(message.client.color)
+            .setColor(message.client.color)
             .setFooter(message.client.footer)
             .addFields({ name: "Liens utliles", value: `
-                [Dashboard](http://green-bot.tk/)-[Inviter le bot](https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=8) - [Support](https://discord.gg/nrReAmApVJ) - [Github](https://github.com/pauldb09/Green-bot)` })
+                [Dashboard](http://green-bot.tk/)-[Inviter le bot](https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&scope=bot&permissions=8) - [Support](https://discord.gg/nrReAmApVJ) - [Github](https://github.com/pauldb09/Green-bot) - [Documentation](https://docs.green-bot.xyz/)` })
 
 
         message.channel.send(embed)
