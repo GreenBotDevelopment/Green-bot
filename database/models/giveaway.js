7b76c2bd693bf91dd4ed5210e344fe9cadcfbdf4
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2021-05-19 16:58:59 +0200

    Delete database/models directory

--- database/models/giveaway.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const rrdb = new mongoose.Schema({
-    serverID: { type: String, required: false },
-    MessageID: { type: String, required: false },
-    requiredRole: { type: String, required: false },
-    requiredInvites: { type: String, required: false },
-    requiredMessages: { type: String, required: false },
-})
-const giveawayModel = module.exports = mongoose.model('giveaway', rrdb)
