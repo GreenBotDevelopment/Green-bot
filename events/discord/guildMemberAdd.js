383b94bb13d3d239f0097c6c3001573359897b10
Pauldb09 <pauldeboissieu@yahoo.com>
2021-09-30 20:00:28 +0200

    📚 Moving the fonts in the util folder

--- events/discord/guildMemberAdd.js
+++ events/discord/guildMemberAdd.js
@@ -7,7 +7,7 @@ const Welcome = require("../../database/models/Welcome"),
     moment = require("moment"),
     Canvas = require("canvas"),
     { registerFont: registerFont } = require("canvas");
-registerFont("./ZenDots-Regular.ttf", { family: "Zen Dots" }), module.exports = {
+registerFont("./util/fonts/ZenDots-Regular.ttf", { family: "Zen Dots" }), module.exports = {
     async execute(e, s) {
         const ping = await Welcome.findOne({ serverID: e.guild.id, reason: "autoping" });
         if (ping && ping.status) {
@@ -19,9 +19,9 @@ registerFont("./ZenDots-Regular.ttf", { family: "Zen Dots" }), module.exports = 
         }
         const ai = await Welcome.findOne({ serverID: e.guild.id, reason: "verification" });
         if (ai && !e.verified) return
-         const settings = await guildData.findOne({ serverID: e.guild.id })
-if(s.log) console.log(settings)
-         let t, r = await Welcome.findOne({ serverID: e.guild.id, reason: "logs" });
+        const settings = await guildData.findOne({ serverID: e.guild.id })
+        if (s.log) console.log(settings)
+        let t, r = await Welcome.findOne({ serverID: e.guild.id, reason: "logs" });
         t = r ? e.guild.channels.cache.get(r.channelID) : null;
         const lang = await e.guild.translatee("WELCOME", settings.lang)
         const a = (new Discord.MessageEmbed).setTitle(lang.title.replace("{username}", e.user.username)).setThumbnail(e.user.displayAvatarURL()).setDescription(lang.desc.replace("{x}", e.guild.memberCount).replace("{date}", moment(e.user.createdTimestamp).locale(settings.lang).format("LL, "))).setFooter(s.footer).setColor("#04781B");
