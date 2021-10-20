a2162c7344c6e36faedf780b542ffff4392b52e1
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-20 17:18:54 +0200

    ✨ - Optimization

--- commands/configuration/youtube-alerts.js
+++ commands/configuration/youtube-alerts.js
@@ -1,68 +1,65 @@
-const Discord = require('discord.js');
 const ms = require('ms');
-const youtubeD = require('../../database/models/youtube');
-Youtube = require("simple-youtube-api"),
-    youtube = new Youtube("AIzaSyCxQ_Vp34-8c5b1BwzE0gkF10jFmcW1DsM");
+const youtubeModel = require('../../database/models/youtube');
+const Youtube = require('simple-youtube-api');
+const youtube = new Youtube('AIzaSyCxQ_Vp34-8c5b1BwzE0gkF10jFmcW1DsM');
+
 module.exports = {
         name: 'youtube-alerts',
-        description: 'Configure le système le système des alertes lors des nouvelles vidéos youtube  . Cette commande inclut un collecteur de messages , vous n\'avez donc pas besoin d\'arguments.',
+        description: 'Configure le système le système des alertes lors des nouvelles vidéos youtube.',
         aliases: ['youtube-system', 'youtube-notifs'],
         guildOnly: true,
         cat: 'configuration',
         permissions: ['MANAGE_GUILD'],
-        botpermissions: ["SEND_MESSAGES", "EMBED_LINKS", ],
+        botpermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
         async execute(message, args, client) {
-            const prompts = await message.translate("YOUTUBE_PROMPTS")
+            const youtube_prompts = await message.translate('YOUTUBE_PROMPTS');
             const ID = message.member.id;
-            const youtubeDB = await youtubeD.findOne({ serverID: message.guild.id })
-            let tip = await message.translate("DASHBOARD")
-            let second = await message.translate("ARGS_TIP")
-            let cfg = await message.translate("ACTUAL_CONFIG")
-            let embed = new Discord.MessageEmbed()
-                .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, size: 512 }))
-                .setColor("#F0B02F")
-                .setTitle(`${message.guild.settings.lang === "fr" ? "Alertes youtube":"Youtube Notifications"}`)
-                .setDescription(tip)
-                .addField(cfg.title, `${cfg.enabled} ${youtubeDB ? youtubeDB.status ? "✅": "❌" : "❌"}\n Youtubeur: ${youtubeDB ? youtubeDB.youtuberName ? youtubeDB.youtuberName : cfg.no :cfg.no}\n${cfg.channel}  ${ youtubeDB ? youtubeDB.channelID ? `<#${youtubeDB.channelID}>` : cfg.no : cfg.no }\nMessage: \n\`\`\`${ youtubeDB ? youtubeDB.message ? `${youtubeDB.message.length > 500 ? youtubeDB.message.slice(0, 500) + '...': youtubeDB.message}` : cfg.no : cfg.no }\`\`\``)
-                .addField(`${message.guild.settings.lang === "fr" ? "`📜` Utilisation":"`📜` Use"}`, second)
-                .setThumbnail(url = message.client.user.displayAvatarURL({ dynamic: true, size: 512 }))
-                .setFooter(message.client.footer, message.client.user.displayAvatarURL({ dynamic: true, size: 512 }))
-        message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } }).then((m) => {
-            m.react("📜")
-            const filter = (reaction, user) => reaction.emoji.name === "📜" && user.id === message.author.id;
-            const collector = m.createReactionCollector({ filter, time: 1000000,max:1 });
-            collector.on('collect', async r =>{
-                const response = await getResponses(message)
-                if (!response || response.cancelled) return message.client.log ? console.log(`Command ${message.content} | Cancelled`) : null
-                const a = await message.translate("CONFIG_OK")
-                let b = `${message.guild.settings.lang === "fr" ? "les alertes lors d'une vidéo youtube":"the youtube alerts"}`
-                const verify = await  youtubeD.findOne({ serverID: message.guild.id,  })
+            const youtubeDB = await youtubeModel.findOne({ serverID: message.guild.id });
+            const dashboard = await message.translate('DASHBOARD');
+            const args_tip = await message.translate('ARGS_TIP');
+            const cfg = await message.translate('ACTUAL_CONFIG');
+
+            message.reply({
+                embeds: [{
+                    color: '#f0b02f',
+                    author: { name: message.author.username, icon_url: message.author.displayAvatarURL({ dynamic: true }) },
+                    title: message.guild.settings.lang === 'fr' ? 'Messages de bienvenue' : 'Welcome messages',
+                    description: dashboard,
+                    thumbnail: { url: client.user.displayAvatarURL({ format: 'png' }) },
+                    fields: [
+                            { name: cfg.title, description: cfg.enabled + ' ' + (youtubeDB && youtubeDB.status ? '✅' : '❌') + '\n Youtubeur: ' + (youtubeDB && youtubeDB.youtuberName ? youtubeDB.youtuberName : cfg.no) + '\n' + cfg.channel + ' ' + (youtubeDB && youtubeDB.channelID ? '<#' + youtubeDB.channelID + '>' : cfg.no) + '\nMessage: \n```' + (youtubeDB && youtubeDB.message ? youtubeDB.message.length > 500 ? youtubeDB.message.slice(0, 500) + '...' : youtubeDB.message : cfg.no) + '```' },
+                            { name: message.guild.settings.lang == 'fr' ? '`📜` Utilisation' : '`📜` Use', description: args_tip }
+                    ],
+                    footer: { text: client.footer, icon_url: client.user.displayAvatarURL() }
+                }],
+                allowedMentions: { repliedUser: false }
+            }).then(m => {
+            m.react('📜');
+            const collector = m.createReactionCollector({ filter: (reaction, user) => reaction.emoji.name == '📜' && user.id === message.author.id, max: 1 });
+            collector.on('collect', async r => {
+                const response = await getResponses(message);
+                if (!response || response.cancelled && client.log) return console.log('Command ' + message.content + ' | Cancelled');
+                const config_ok = await message.translate('CONFIG_OK');
+                const youtube_alerts = message.guild.settings.lang == 'fr' ? 'les alertes lors d\'une vidéo youtube' : 'the youtube alerts';
+                const verify = await  youtubeModel.findOne({ serverID: message.guild.id });
                 if (verify) {
-                    const newchannel = await  youtubeD.findOneAndUpdate({ serverID: message.guild.id,  }, { $set: { channelID: response.channel.id,  message: response.message, status: response.status, youtuberID:response.ytID, youtuberName:response.name, } }, { new: true });
-                    message.succesMessage(a.replace("{x}",b));
-            } else {
-                const verynew = new youtubeD({
-                    serverID: `${message.guild.id}`,
-                    channelID: `${response.channel.id}`,
-                  youtuberName: `${response.name}`,
-                    message: `${response.message}`,
-                    status:  `${response.status}`,
-                    youtuberID:`${response.ytID}`,
-                }).save();
-              
-                            message.succesMessage(a.replace("{x}",b));
-                        
-            }
+                    const newchannel = await youtubeModel.findOneAndUpdate({ serverID: message.guild.id }, { $set: { channelID: response.channel.id, message: response.message, status: response.status, youtuberID:response.ytID, youtuberName:response.name } }, { new: true });
+                    message.succesMessage(config_ok.replace('{x}', youtube_alerts));
+                } else {
+                    const verynew = new youtubeModel({
+                        serverID: message.guild.id,
+                        channelID: response.channel.id,
+                        youtuberName: response.name,
+                        message: response.message,
+                        status: response.status,
+                        youtuberID: response.ytID,
+                    }).save();
+                    message.succesMessage(config_ok.replace('{x}', b));
+                }
             });
             collector.on('end', collected => m.reactions.removeAll());
-    
-    
-    
-    
         });
     
-      
-
 /**
  * Get the youtube channel id from an url
  * @param {string} url The URL of the youtube channel
@@ -123,23 +120,23 @@ message.channel.send(`**${okk}**`)
                 break;
             }
             if (i === 0) {
-                const ll = await message.translate("ENA/DISA")
+                const enable_or_disable = await message.translate('ENA/DISA');
 
                 if (content.toLowerCase() === 'enable') {
                     responses.status = true;
                 } else if (content.toLowerCase() === 'disable') {
                     responses.status = null;
-                    const verify = await  youtubeD.findOne({ serverID: message.guild.id,  })
+                    const verify = await youtubeModel.findOne({ serverID: message.guild.id });
                     if (verify) {
-                        const newchannel = await  youtubeD.findOneAndUpdate({ serverID: message.guild.id,  }, { $set: { status: null } }, { new: true });
+                        const newchannel = await youtubeModel.findOneAndUpdate({ serverID: message.guild.id,  }, { $set: { status: null } }, { new: true });
 
-                        return message.succesMessage(ll.succes);
+                        return message.succesMessage(enable_or_disable.succes);
 
                     }
-                    return message.succesMessage(ll.disable);
+                    return message.succesMessage(enable_or_disable.disable);
 
                 } else {
-                    return m.errorMessage(ll.err)
+                    return m.errorMessage(enable_or_disable.err);
                 }
 
             }
@@ -147,21 +144,21 @@ message.channel.send(`**${okk}**`)
                 let channel = m.mentions.channels.first() || message.guild.channels.cache.get(content) || message.guild.channels.cache.filter(m => m.type === "GUILD_TEXT" && m.name.includes(args.join(" "))).first();
                     if (channel && channel.type === 'GUILD_TEXT' && channel.guild.id === message.guild.id) {
                     if (!channel.viewable||!channel.permissionsFor(message.guild.me).has('SEND_MESSAGES') || !channel.permissionsFor(message.guild.me).has('EMBED_LINKS') ) {
-                        let a = await message.translate("CHANNEL_PERMS")
-                        return m.errorMessage(a)
+                        const channel_permissions = await message.translate('CHANNEL_PERMS');
+                        return m.errorMessage(channel_permissions)
                         break;
                     }
                     responses.channel = channel;
                 } else {
-                    let errorChannel = await message.translate("ERROR_CHANNEL")
-                    return m.errorMessage(errorChannel)
+                    const error_channel = await message.translate('ERROR_CHANNEL')
+                    return m.errorMessage(error_channel)
                     break;
                 }
             }
             if (i === 2) {
                 if (content.length > 1000 || content.length < 2) {
-                    let numberErr = await message.translate("MESSAGE_ERROR")
-                    return m.errorMessage(numberErr.replace("{amount}", "2").replace("{range}", "1000"))
+                    const number_error = await message.translate('MESSAGE_ERROR')
+                    return m.errorMessage(number_error.replace('{amount}', '2').replace('{range}', '1000'))
                     break;
                 } else {
                     responses.message = content;
@@ -169,9 +166,9 @@ message.channel.send(`**${okk}**`)
             }
             if (i === 3) {
                 let channelInfos = await getYoutubeChannelInfos(content);
-if(!channelInfos) return m.errorMessage(`${message.guild.settings.lang === "fr" ? "Veuillez fournir un nom valide de chaîne youtube ou un lien de chaîne valide.":"Please provide a valid youtube channel name or a valid channel link."}`)
-responses.ytID = channelInfos.id
-responses.name =content;
+                if (!channelInfos) return m.errorMessage(`${message.guild.settings.lang === "fr" ? "Veuillez fournir un nom valide de chaîne youtube ou un lien de chaîne valide.":"Please provide a valid youtube channel name or a valid channel link."}`)
+                    responses.ytID = channelInfos.id
+                responses.name =content;
             }
         }
         return responses;
