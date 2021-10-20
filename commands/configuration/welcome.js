099dd8ca30aa32071aef6ced7d45ba562175f093
squarfiuz <70509576+squarfiuz@users.noreply.github.com>
2021-10-20 16:32:04 +0200

    Update welcome.js
    ✨ - Optimization

--- commands/configuration/welcome.js
+++ commands/configuration/welcome.js
@@ -10,124 +10,129 @@ module.exports = {
         botpermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
         async execute(message, args, client) {
             const ID = message.member.id;
-            const prompts = await message.translate("WELCOME_PROMPTS")
-            const welcomeDB = await Welcome.findOne({ serverID: message.guild.id, reason: `welcome` })
-            let tip = await message.translate("DASHBOARD")
-            let second = await message.translate("ARGS_TIP")
-            let cfg = await message.translate("ACTUAL_CONFIG")
-            let embed = new Discord.MessageEmbed()
-                .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, size: 512 }))
-                .setColor("#F0B02F")
-                .setTitle(`${message.guild.settings.lang === "fr" ? "Messages de bienvenue":"Welcome messages"}`)
-                .setDescription(tip)
-                .addField(cfg.title, `${cfg.enabled}${welcomeDB ? welcomeDB.status ? '✅' : '❌' : '❌'}\n image: ${welcomeDB ? welcomeDB.image ? '✅' : '❌' : '❌'}\n${cfg.channel}  ${ welcomeDB ? welcomeDB.channelID ? `<#${welcomeDB.channelID}>` : cfg.no : cfg.no }\nMessage: \n\`\`\`${ welcomeDB ? welcomeDB.message ? `${welcomeDB.message.length > 500 ? welcomeDB.message.slice(0, 500) + '...':welcomeDB.message}` : cfg.no : cfg.no }\`\`\``)
-                .addField(`${message.guild.settings.lang === "fr" ? "`📜` Utilisation":"`📜` Use"}`, second)
-        .setThumbnail(url = message.client.user.displayAvatarURL({ dynamic: true, size: 512 }))
-        .setFooter(message.client.footer, message.client.user.displayAvatarURL({ dynamic: true, size: 512 }))
-        message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } }).then(m => {
-            m.react('📜');
-            const collector = m.createReactionCollector({ filter: (reaction, user) => reaction.emoji.name === "📜" && user.id === message.author.id, max:1 });
-            collector.on('collect', async r => {
-                const response = await getResponses(message)
-                if (!response || response.cancelled) return message.client.log ? console.log(`Command ${message.content} | Cancelled`) : null
-                const a = await message.translate("CONFIG_OK");
-                let b = message.guild.settings.lang === 'fr' ? 'les messages de bienvenue' : 'the welcome messages';
-                const verify = await Welcome.findOne({ serverID: message.guild.id, reason: 'welcome' });
-                if (verify) {
-                        const newchannel = await Welcome.findOneAndUpdate({ serverID: message.guild.id, reason: `welcome` }, { $set: { channelID: response.channel.id, reason: `welcome `, message: response.message, status: response.status, image: response.image ? true : false, } }, { new: true });
-                        message.succesMessage(a.replace('{x}', b));
-                } else {
-                    const verynew = new Welcome({
-                        serverID: message.guild.id,
-                        channelID: response.channel.id,
-                        reason: 'welcome',
-                        message: response.message,
-                        status:  response.status,
-                        image:  response.image,
-                    }).save();
-                    message.succesMessage(a.replace("{x}",b));
-                }
-            });
-            collector.on('end', collected => m.reactions.removeAll());
-        });
-    async function getResponses(message) {
-        const validTime = /^\d+(s|m|h|d)$/;
-        const validNumber = /^\d+/;
-        const responses = {}
+            const prompts = await message.translate('WELCOME_PROMPTS');
+            const welcomeDB = await Welcome.findOne({ serverID: message.guild.id, reason: 'welcome' });
+            const dashboard = await message.translate('DASHBOARD');
+            const args_tip = await message.translate('ARGS_TIP');
+            const cfg = await message.translate('ACTUAL_CONFIG');
+                message.reply({
+                        embeds: [{
+                                color: '#f0b02f',
+                                author: { name: message.author.username, icon_url: message.author.displayAvatarURL({ dynamic: true }) },
+                                title: message.guild.settings.lang === 'fr' ? 'Messages de bienvenue' : 'Welcome messages',
+                                description: dashboard,
+                                thumbnail: { url: client.user.displayAvatarURL({ format: 'png' }) },
+                                fields: [
+                                        { name: cgf.title, description: cfg.enabled + (welcomeDB && welcomeDB.status ? '✅' : '❌') + '\nImage: ' + (welcomeDB && welcomeDB.image ? '✅'  : '❌') + '\n' + cfg.channel + ' ' + (welcomeDB && welcomeDB.channelID ? '<#' + welcomeDB.channelID + '>' : cfg.no) + '\nMessage: \n```' + (welcomeDB && welcomeDB.message ? (welcomeDB.message.length > 500 ? welcomeDB.message.slice(0, 500) + '...' : welcomeDB.message) : cfg.no) + '```' },
+                                        { name: message.guild.settings.lang == 'fr' ? '`📜` Utilisation' : '`📜` Use', description: second }
+                                ],
+                                footer: { text: client.footer, icon_url: client.user.displayAvatarURL() }
+                        }],
+                        allowedMentions: { repliedUser: false }
+                }).then(m => {
+                    m.react('📜');
+                    const collector = m.createReactionCollector({ filter: (reaction, user) => reaction.emoji.name === "📜" && user.id === message.author.id, max:1 });
+                    collector.on('collect', async r => {
+                        const response = await getResponses(message)
+                        if (!response || response.cancelled) return client.log ? console.log('Command ' + message.content + ' | Cancelled') : null;
+                        const config_ok = await message.translate('CONFIG_OK');
+                        const welcome_messages = message.guild.settings.lang == 'fr' ? 'les messages de bienvenue' : 'the welcome messages';
+                        const verify = await Welcome.findOne({ serverID: message.guild.id, reason: 'welcome' });
+                        if (verify) {
+                                const newchannel = await Welcome.findOneAndUpdate({ serverID: message.guild.id, reason: `welcome` }, { $set: { channelID: response.channel.id, reason: `welcome `, message: response.message, status: response.status, image: response.image ? true : false, } }, { new: true });
+                                message.succesMessage(config_ok.replace('{x}', welcome_messages));
+                        } else {
+                            const verynew = new Welcome({
+                                serverID: message.guild.id,
+                                channelID: response.channel.id,
+                                reason: 'welcome',
+                                message: response.message,
+                                status:  response.status,
+                                image:  response.image,
+                            }).save();
+                            message.succesMessage(config_ok.replace('{x}', welcome_messages));
+                        }
+                    });
+                    collector.on('end', collected => m.reactions.removeAll());
+                });
+            async function getResponses(message) {
+                const validTime = /^\d+(s|m|h|d)$/;
+                const validNumber = /^\d+/;
+                const responses = {}
 
-        let can = await message.translate("CAN_CANCEL")
-            for (let i = 0; i < prompts.length; i++) {
-                await message.mainMessageT(`${prompts[i]}\n\n${can}`);
-        const filter = m => m.author.id === ID;
-        const response =  await message.channel.awaitMessages({ filter, max: 1,})
-            const { content } = response.first();
-            const m = response.first();
-            if (content.toLowerCase() === "cancel") {
-                const okk = await message.translate("CANCELED");
-                responses.cancelled = true;
+                let can = await message.translate("CAN_CANCEL")
+                    for (let i = 0; i < prompts.length; i++) {
+                        await message.mainMessageT(`${prompts[i]}\n\n${can}`);
+                const filter = m => m.author.id === ID;
+                const response =  await message.channel.awaitMessages({ filter, max: 1,})
+                    const { content } = response.first();
+                    const m = response.first();
+                    if (content.toLowerCase() === "cancel") {
+                        const canceled = await message.translate('CANCELED');
+                        responses.cancelled = true;
 
-                message.channel.send('**' + okk + '**');
-                return responses;
-                break;
-            }
-            if (i === 0) {
-                const ll = await message.translate('ENA/DISA');
-
-                if (content.toLowerCase() === 'enable') {
-                    responses.status = true;
-                } else if (content.toLowerCase() === 'disable') {
-                    responses.status = null;
-
-                    const verify = await Welcome.findOne({ serverID: message.guild.id, reason: 'welcome' });
-                    if (verify) {
-                        const newchannel = await Welcome.findOneAndUpdate({ serverID: message.guild.id, reason: 'welcome' }, { $set: { status: null } }, { new: true });
-
-                        return message.succesMessage(ll.succes);
-
-                    }
-                    return message.succesMessage(ll.disable);
-
-                } else {
-                    return m.errorMessage(ll.err);
-                }
-
-            }
-            if (i === 1) {
-                let channel = m.mentions.channels.first() || message.guild.channels.cache.get(content) || message.guild.channels.cache.filter(m => m.type === "GUILD_TEXT" && m.name.includes(args.join(" "))).first();
-                    if (channel && channel.type === 'GUILD_TEXT' && channel.guild.id === message.guild.id) {
-                    if (!channel.viewable||!channel.permissionsFor(message.guild.me).has('SEND_MESSAGES') || !channel.permissionsFor(message.guild.me).has('EMBED_LINKS') ) {
-                        const a = await message.translate('CHANNEL_PERMS');
-                        return m.errorMessage(a);
+                        message.channel.send('**' + canceled + '**');
+                        return responses;
                         break;
                     }
-                    responses.channel = channel;
-                } else {
-                    const errorChannel = await message.translate('ERROR_CHANNEL');
-                    return m.errorMessage(errorChannel);
-                    break;
-                }
-            }
-            if (i === 2) {
-                if (content.length > 2000 || content.length < 1) {
-                    let numberErr = await message.translate('MESSAGE_ERROR');
-                return m.errorMessage(numberErr.replace('{amount}', '2').replace('{range}', '2000'));
-                    break;
-                } else {
-                    responses.message = content;
-                }
-            }
-            if (i === 3) {
-                if (content.toLowerCase() === 'image') {
-                    responses.image = true;
-                } else if (content.toLowerCase() === 'text') {
-                    responses.image = null;
-                } else {
-                    return m.errorMessage(message.guild.settings.lang === 'fr' ? 'Veuillez fournir l\'argument attendu: **image** ou **text**.' : 'Please provide the expected argument: **image** or **text**.');
-                }
+                    if (i === 0) {
+                        const enable_or_disable = await message.translate('ENA/DISA');
 
+                        if (content.toLowerCase() === 'enable') {
+                            responses.status = true;
+                        } else if (content.toLowerCase() === 'disable') {
+                            responses.status = null;
+
+                            const verify = await Welcome.findOne({ serverID: message.guild.id, reason: 'welcome' });
+                            if (verify) {
+                                const newchannel = await Welcome.findOneAndUpdate({ serverID: message.guild.id, reason: 'welcome' }, { $set: { status: null } }, { new: true });
+
+                                return message.succesMessage(enable_or_disable.succes);
+
+                            }
+                            return message.succesMessage(enable_or_disable.disable);
+
+                        } else {
+                            return m.errorMessage(enable_or_disable.err);
+                        }
+
+                    }
+                    if (i === 1) {
+                        let channel = m.mentions.channels.first() || message.guild.channels.cache.get(content) || message.guild.channels.cache.filter(m => m.type === "GUILD_TEXT" && m.name.includes(args.join(" "))).first();
+                            if (channel && channel.type === 'GUILD_TEXT' && channel.guild.id === message.guild.id) {
+                            if (!channel.viewable||!channel.permissionsFor(message.guild.me).has('SEND_MESSAGES') || !channel.permissionsFor(message.guild.me).has('EMBED_LINKS') ) {
+                                const channel_permissions = await message.translate('CHANNEL_PERMS');
+                                return m.errorMessage(channel_permissions);
+                                break;
+                            }
+                            responses.channel = channel;
+                        } else {
+                            const error_channel = await message.translate('ERROR_CHANNEL');
+                            return m.errorMessage(error_channel);
+                            break;
+                        }
+                    }
+                    if (i === 2) {
+                        if (content.length > 2000 || content.length < 1) {
+                                const number_error = await message.translate('MESSAGE_ERROR');
+                                return m.errorMessage(number_error.replace('{amount}', '2').replace('{range}', '2000'));
+                                break;
+                        } else {
+                            responses.message = content;
+                        }
+                    }
+                    if (i === 3) {
+                        if (content.toLowerCase() === 'image') {
+                            responses.image = true;
+                        } else if (content.toLowerCase() === 'text') {
+                            responses.image = null;
+                        } else {
+                            return m.errorMessage(message.guild.settings.lang === 'fr' ? 'Veuillez fournir l\'argument attendu: **image** ou **text**.' : 'Please provide the expected argument: **image** or **text**.');
+                        }
+
+                    }
+                }
+                return responses;
             }
-        }
-        return responses;
-    }
-},
+        },
 };
