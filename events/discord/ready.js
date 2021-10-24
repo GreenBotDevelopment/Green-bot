3389d8d637cdf42c5bc9d6441f95e6bf13344502
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2021-10-24 12:24:53 +0200

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -5,9 +5,9 @@ module.exports = {
         const crons = require("../../util/crons")
         crons.handleReminds(client)
         crons.checkBirthdays(client)
-        crons.checkYoutubeVideos(client)
+       // crons.checkYoutubeVideos(client)
         setInterval(async() => {
-            await crons.checkYoutubeVideos(client)
+         //   await crons.checkYoutubeVideos(client)
         }, 6e4);
         if (config.database.cached) {
             const { refreshDB } = require("../../util/crons")
