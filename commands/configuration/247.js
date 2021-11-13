bcc8e13cf9b4142978d897f9733aeb73c5b68137
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2021-11-13 15:21:13 +0100

    Update 247.js

--- commands/configuration/247.js
+++ commands/configuration/247.js
@@ -1,4 +1,3 @@
-const guildData = require('../../database/models/guildData');
 module.exports = {
     name: '24/7',
     description: 'Enable/Disable The 24h/7 mode',
@@ -14,28 +13,10 @@ module.exports = {
             guildDB.h24 = true;
             guildDB.save();
             message.succesMessage(lang.enabled, true);
-            /*    const findSchema = await voiceSchema.findOne({ serverID: message.guild.id });
-                if (findSchema) {
-                    findSchema.channelID = message.member.voice.id;
-                    findSchema.save();
-                } else {
-                    const newSchema = new voiceSchema({
-                        serverID: message.guild.id,
-                        channelID: message.member.voice.id
-                    })
-                    newSchema.save();
-                }
-                */
         } else {
             guildDB.h24 = null;
             guildDB.save();
             message.succesMessage(lang.disabled, true);
-            /*  const findSchema = await voiceSchema.findOne({ serverID: message.guild.id });
-             if (findSchema) {
-                 findSchema.channelID = null;
-                 findSchema.save();
-             }
-             */
         };
     },
 };
