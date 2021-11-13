21ae091cebc173369c6774881d4cace300b4e9b6
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2021-11-13 16:01:50 +0100

    🧷 Fix

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 const fs = require("fs"),
-    config = require("./config.js");
-if (config.checkConfig) {
-    const { checkConfig } = require("./util/functions")
-    checkConfig(config).then(result => {
-        if (result) {
-            console.log("Config error")
-            process.exit(0);
-        }
+    { getFreeClientID: getFreeClientID, setToken: setToken } = require("play-dl"),
+    { Client: Client, Intents: Intents, Collection: Collection, MessageEmbed } = require("discord.js"),
+    client = new Client({
+        messageCacheMaxSize: 20,
+        intents: [Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        ]
+    }),
+    util = require("util"),
+    { Player } = require("discord-player"),
+    readdir = util.promisify(fs.readdir),
+    mongoose = require("mongoose");
+client.config = require("./config.js"),
+    client.cooldowns = [],
+    client.footer =
+    client.config.footer,
+    client.commands = new Collection,
+    client.player = new Player(client, client.config.player),
+    getFreeClientID().then(e => { setToken({ soundcloud: { client_id: e } }) }),
+    mongoose.connect(client.config.database.mongoURL, client.config.database.options).then(() => { console.log("[MongoDB]: Ready") })
+    .catch(e => { console.log("[MongoDB]: Error\n" + e) });
+const init = async function() {
+    fs.readdirSync("./commands").filter(e => e.endsWith(".js"));
+    const e = await readdir("./commands/");
+    console.log(`[Commands] ${e.length} Categories loaded.`), e.forEach(async e => {
+        (await readdir("./commands/" + e + "/")).filter(e => "js" === e.split(".").pop()).forEach(n => {
+            const t = require(`./commands/${e}/${n}`);
+            client.commands.set(t.name, t)
+        })
+    });
+    const n = await readdir("./events/discord");
+    console.log(`[Events] ${n.length} events loaded.`), n.forEach(e => {
+        const n = e.split(".")[0],
+            t = require(`./events/discord/${e}`);
+        client.on(n, (...e) => t.execute(...e, client)), delete require.cache[require.resolve(`./events/discord/${e}`)]
     })
-
-}
-const GreenBot = require("./base/GreenBot");
-const client = new GreenBot({
-    fetchAllMembers: !0,
-    autoReconnect: !0,
-    messageCacheMaxSize: 20,
-    partials: ["MESSAGE", "CHANNEL", "GUILD_MEMBER", "REACTION", "GUILD_VOICE_STATES"],
-    intents: [
-        "GUILD_MEMBERS",
-        "GUILDS",
-        "GUILD_VOICE_STATES",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "GUILD_BANS",
-        "GUILD_INVITES",
-        "GUILD_VOICE_STATES",
-        "DIRECT_MESSAGES",
-        "GUILD_INTEGRATIONS",
-        "GUILD_WEBHOOKS",
-        "GUILD_MESSAGE_TYPING",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING",
-        "GUILD_MESSAGE_REACTIONS",
-    ],
-    allowedMentions: {
-        parse: ["users", "roles"]
-    },
-});
-const util = require("util");
-require("./util/extenders.js");
-const readdir = util.promisify(fs.readdir);
-   client.player.on("trackStart", async(queue, track) => {
+    client.player.on("trackStart", async(queue, track) => {
         if (!queue.metadata) return console.log("Not metadata")
         if (queue.metadata.controller) {
             const embed = new MessageEmbed()
@@ -117,35 +115,5 @@ const readdir = util.promisify(fs.readdir);
         return
 
     })
-const init = async() => {
-        fs.readdirSync("./commands").filter((e) => e.endsWith(".js"));
-        const e = await readdir("./commands/");
-        console.log(`[Commands] ${e.length} Categories loaded.`),
-            e.forEach(async(e) => {
-                (await readdir("./commands/" + e + "/"))
-                .filter((e) => "js" === e.split(".").pop())
-                    .forEach((t) => {
-                        const n = require(`./commands/${e}/${t}`);
-                        client.commands.set(n.name, n);
-                    });
-            });
-        const t = await readdir("./events/discord");
-        console.log(`[Events] ${t.length} events loaded.`),
-            t.forEach((e) => {
-                const t = e.split(".")[0];
-                const n = require(`./events/discord/${e}`);
-                client.on(t, (...e) => n.execute(...e, client)), delete require.cache[require.resolve(`./events/discord/${e}`)];
-            });
-        mongoose = require("mongoose");
-        mongoose
-            .connect(config.database.MongoURL, { useNewUrlParser: !0, useUnifiedTopology: !0, useFindAndModify: !1 })
-            .then(() => {
-                console.log("[MongoDB] : Ready");
-            })
-            .catch((e) => {
-                console.log("MongoDB Error:" + e);
-            });
-    };
-init(), client.login(config.token).catch(err => {
-    console.log("[Discord login] Please provide a valid discord bot token\n" + err + "")
-});
+};
+init(), client.login(client.config.token).catch(e => { console.log("[Discord login]: Please provide a valid discord bot token\n" + e) });
