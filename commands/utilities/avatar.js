265c0699a4b1b5eb50425f5194820e8aee652767
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-03-20 16:20:47 +0100

    Fix optimisation default member
    Fix optimisation default member.
    Fix duplicate generate displayAvatarUrl member
    Change visual :)

--- commands/utilities/avatar.js
+++ commands/utilities/avatar.js
@@ -4,37 +4,45 @@ module.exports = {
     aliases: ['profilepic', 'pic', 'pp', 'pdp'],
     cat: 'utilities',
     async execute(message, args, client, guildDB) {
-        let member;
+        let member = message.member;
         if (args.length) {
             member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.filter(m => m.user.tag.toLowerCase().includes(args[0].toLowerCase()) || m.displayName.toLowerCase().includes(args[0].toLowerCase()) || m.user.username.toLowerCase().includes(args[0].toLowerCase())).first()
             if (!member) {
-                let err = await message.translate("ERROR_USER", guildDB.lang)
-                return message.errorMessage(err)
+                const err = await message.translate("ERROR_USER", guildDB.lang)
+                return message.errorMessage(err);
             }
-        } else {
-            member = message.member
         }
-        let a = await message.translate("AVATAR", guildDB.lang)
-        let b = await message.translate("AVATAR_DESC", guildDB.lang)
-        message.channel.send({
-            embeds: [{
-                author: {
-                    name: `${a}${member.user.tag}`,
-                    icon_url: member.user.displayAvatarURL({ dynamic: true, size: 512 }),
-                    url: client.config.links.invite
+        const a = await message.translate("AVATAR", guildDB.lang);
+        const b = await message.translate("AVATAR_DESC", guildDB.lang);
+        const memberAvatarUrl = member.user.displayAvatarURL({
+            dynamic: true,
+            size: 512
+        });
+        return message.channel.send({
+            embeds: [
+                {
+                    author: {
+                        name: `${a}${member.user.tag}`,
+                        icon_url: memberAvatarUrl,
+                        url: client.config.links.invite,
+                    },
+                    color: guildDB.color,
+                    image: {
+                        url: memberAvatarUrl,
+                    },
+                    footer: {
+                        text: message.client.footer,
+                        icon_url: message.client.user.displayAvatarURL({
+                            dynamic: true,
+                            size: 512
+                        }),
+                    },
+                    description: b.replace("{url}", memberAvatarUrl),
                 },
-                color: guildDB.color,
-                image: {
-                    url: member.user.displayAvatarURL({ dynamic: true, size: 512 })
-                },
-                footer: {
-                    text: message.client.footer,
-                    icon_url: message.client.user.displayAvatarURL({ dynamic: true, size: 512 })
-                },
-                description: b.replace("{url}", member.user.displayAvatarURL({ dynamic: true, size: 512 }))
-
-            }],
-            allowedMentions: { repliedUser: false }
-        })
+            ],
+            allowedMentions: {
+                repliedUser: false
+            },
+        });
     },
 };
