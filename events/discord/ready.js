6deba2da275716bc765a2758c49c4968628b913a
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-03-20 16:10:10 +0100

    Fix activities bot and optimisation activities

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,21 +1,24 @@
 const config = require('../../config.js');
 module.exports = {
     async execute(client) {
-        console.log('[Bot] Ready');
         const DBL = require('dblapi.js');
         client.dbl = new DBL(config.topgg.token, client);
         const activities = [
-            { name: 'green-bot.app • *help', type: 'WATCHING' },
-            { name: 'green-bot.app • *help', type: 'WATCHING' }
+            { name: 'green-bot.app • *help', type: 'WATCHING' }, // 0
+            { name: 'green-bot.app • *help', type: 'WATCHING' }, // 1
+            { name: 'green-bot.app', type: 'WATCHING' }, // 2
         ];
-        client.user.setActivity(activities[0].name, { type: 'WATCHING' });
-        let activity = 1;
+        client.user.setActivity(activities[0].name, {
+            type: activities[0].type
+        });
+        let activity = 0;
         setInterval(async() => {
-            activities[2] = { name: 'green-bot.app', type: 'WATCHING' };
-            activities[3] = { name: 'green-bot.app', type: 'WATCHING' };
-            if (activity > 3) activity = 0;
-            client.user.setActivity(activities[activity].name, { type: 'WATCHING' });
+            if (activity >= 2) activity = 0;
+            client.user.setActivity(activities[activity].name, {
+                type: activities[activity].type
+            });
             activity++;
         }, 30000);
-    }
+        console.log('[Bot] Bot is ready!');
+    },
 };
