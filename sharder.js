2dd02ad795bca2f677b597de6a83608319ae4eec
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-04-10 13:02:12 +0200

    Update sharder.js
    Optimisation

--- sharder.js
+++ sharder.js
@@ -1,9 +1,11 @@
 const { ShardingManager } = require("discord.js");
-const config = require("./config")
+const { token, shards }  = require("./config");
 const manager = new ShardingManager("./index.js", {
     token: config.token,
     shardArgs: process.argv,
-    totalShards: config.shards ? config.shards : "auto",
+    totalShards: shards ?? "auto",
 });
 console.log("[Shards] Starting spawn");
-manager.spawn({ timeout: -1 });
+manager.spawn({
+    timeout: -1
+});
