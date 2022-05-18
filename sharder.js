3e55802cf993f69ad7c024f3b8972aa2c9cbf148
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2022-05-18 20:00:47 +0200

    Update sharder.js
    Retrun error i don't incluse token in file config.js

--- sharder.js
+++ sharder.js
@@ -1,11 +1,13 @@
 const { ShardingManager } = require("discord.js");
 const { token, shards }  = require("./config");
-const manager = new ShardingManager("./index.js", {
-    token: token,
-    shardArgs: process.argv,
-    totalShards: shards ?? "auto",
-});
-console.log("[Shards] Starting spawn...");
-manager.spawn({
-    timeout: -1
-});
+if(token) {
+    const manager = new ShardingManager("./index.js", {
+        token: token,
+        shardArgs: process.argv,
+        totalShards: shards ?? "auto",
+    });
+    console.log("[Shards] Starting spawn...");
+    manager.spawn({
+        timeout: -1
+    });
+} else return console.log("[Error] Please add your token bot in file config.");
