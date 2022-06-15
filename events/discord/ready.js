7ddbf8dce1698f0a1ec3e70ea765b047ac1b80ef
pauldb09 <70904183+pauldb09@users.noreply.github.com>
2022-06-15 13:29:21 +0200

    Update ready.js

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -3,22 +3,6 @@ module.exports = {
     async execute(client) {
         const DBL = require('dblapi.js');
         client.dbl = new DBL(config.topgg.token, client);
-        const activities = [
-            { name: 'green-bot.app • *help', type: 'WATCHING' }, // 0
-            { name: 'Rich quality music', type: 'PLAYING' }, // 1
-            { name: 'green-bot.app', type: 'WATCHING' }, // 2
-        ];
-        let activity = 0; // default activity
-        client.user.setActivity(activities[activity].name, {
-            type: activities[activity].type
-        });
-        setInterval(async() => {
-            if (activity > activities.length) activity = 0; // Reset activity
-            client.user.setActivity(activities[activity].name, {
-                type: activities[activity].type
-            });
-            activity++;
-        }, 30000);
         return console.log('[Bot] Bot is ready!');
     },
 };
