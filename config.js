51edeed18c067308afd7d786fd33455fcded9497
Pauldb09 <pauldeboissieu@yahoo.com>
2022-06-15 21:14:53 +0200

    Update readme

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 module.exports = {
+
+    // Complete it carefuly else it won't work, check the readme for more informations
     lavalink: [
-        { name: "1", url: "LAVALINK_IP:PORT", auth: "LAVALINK_AUTH" },
-    ],
-    spotify: { clientId: "", clientSecret: "", cacheResults: !0, cacheLifetime: 36e5 }
+            { name: "1", url: "LAVALINK_IP:PORT", auth: "LAVALINK_AUTH" },
+        ]
+        // Optional, for loading playlists with more than 100 tracks ( Else using a scaper )
+    spotify: { clientId: "", clientSecret: "", cacheResults: !0, cacheLifetime: 36e5 },
+    // The wrapper used to connect to the lavalink server: https://deivu.github.io/Shoukaku
     shoukaku: { moveOnDisconnect: !0, resumable: !0, closedEventDelay: 1e3, resumableTimeout: 500, reconnectTries: 5, restTimeout: 3e4 },
+    // The database. Create a database on Atlas/ Your vps and copy the connection url here. Additional informations: https://www.mongodb.com/
     mongo: { url: "mongodb://localhost:27017/test", options: { maxPoolSize: 200, useNewUrlParser: !0, useUnifiedTopology: !0, autoIndex: !0, serverSelectionTimeoutMS: 45e3, socketTimeoutMS: 8e5, keepAlive: !0, connectTimeoutMS: 5e5 } },
+    // The discord bot token for discord developers: https://discord.com/developpers/bots
     token: "DISCORD_BOT_TOKEN",
     premiumUrl: "", // Let this blank because this github is for non-commercial use 
 };
