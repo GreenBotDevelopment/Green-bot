0de967f5480d918dbe0a0d4c3393344b709d5ac4
Pauldb09 <pauldeboissieu@yahoo.com>
2022-06-17 17:12:53 +0200

    Update fix.js

--- src/commands/config/fix.js
+++ src/commands/config/fix.js
@@ -25,7 +25,7 @@ class Skip extends BaseCommand {
             e.errorMessage("Please return a node name (1-4)"),
             e.dispatcher.player.connection && 3 != e.dispatcher.player.connection.state ?
             "rotterdam" !== e.member.voice.channel.rtcRegion ?
-            (e.member.voice.channel.setRTCRegion("rotterdam").catch((r) => e.errorMessage("I can't change your voice channel region because I don't have the permission to!")),
+            (e.member.voice.channel.setRTCRegion("rotterdam").catch(() => e.errorMessage("I can't change your voice channel region because I don't have the permission to!")),
                 e.successMessage("Changed voice channel region to **europe**")) :
             e.errorMessage("No fix can be made!") :
             (e.dispatcher.queue.length, e.dispatcher.player.connection.reconnect().then(() => e.dispatcher.player.resume()), e.successMessage(`Reconnected the bot in <#${e.dispatcher.player.connection.channelId}>`))
